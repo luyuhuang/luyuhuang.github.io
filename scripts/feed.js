4057882430fb8da337ab5fa05bd05a28b81de887
luyuhuang <luyu_huang@foxmail.com>
2025-07-01 12:17:27 +0800

    improve feed generator

--- scripts/feed.js
+++ scripts/feed.js
@@ -1,47 +1,28 @@
 const nunjucks = require('nunjucks');
-const { spawn } = require('child_process');
 const { readFileSync } = require('fs');
 const { encodeURL, full_url_for } = require('hexo-util');
+const { parse } = require('node-html-parser');
 
 hexo.extend.generator.register('feed', async locals => {
-    const args = ['-f', 'markdown-smart', '-t', 'html-smart', '--mathjax', '--no-highlight'];
-    const tasks = locals.posts.filter(post => post.draft !== true).sort('-date').limit(10).map(post => {
-        return new Promise((resolve, reject) => {
-            const child = spawn('pandoc', args);
-            child.stdin.write(post._content);
-            child.stdin.end();
-            let result = '', error = '';
-            child.stdout.on('data', data => {
-                result += data.toString('utf8');
-            });
-            child.stderr.on('data', data => {
-                error += data.toString('utf8');
-            });
-            child.on('exit', code => {
-                if (code === 0) {
-                    resolve({
-                        title: post.title,
-                        permalink: post.permalink,
-                        updated: post.updated,
-                        date: post.date,
-                        content: result,
-                        image: post.image,
-                        categories: post.categories,
-                        tags: post.tags,
-                    });
-                } else {
-                    reject(error);
-                }
-            });
+    const posts = locals.posts.filter(post => post.draft !== true).sort('-date').limit(10).map(post => {
+        const root = parse(post.content);
+        root.querySelectorAll('figure.highlight').forEach(elem => {
+            const code = elem.querySelectorAll('table td.code > pre').toString();
+            elem.replaceWith(code);
         });
+        return {
+            title: post.title,
+            permalink: post.permalink,
+            updated: post.updated,
+            date: post.date,
+            content: root.toString(),
+            summary: root.textContent.substring(0, 150),
+            image: post.image,
+            categories: post.categories,
+            tags: post.tags,
+        };
     });
 
-    const env = new nunjucks.Environment();
-    env.addFilter('uriencode', str => encodeURL(str));
-    env.addFilter('formatUrl', str => full_url_for.call(hexo, str));
-    const template = nunjucks.compile(readFileSync('source/_feed.xml', 'utf8'), env);
-
-    const posts = await Promise.all(tasks);
     const config = hexo.config;
     let url = hexo.config.url;
     if (url[url.length - 1] !== '/') {
@@ -51,6 +32,10 @@ hexo.extend.generator.register('feed', async locals => {
     const path = 'feed.xml';
     const feed_url = full_url_for.call(hexo, path);
     
+    const env = new nunjucks.Environment();
+    env.addFilter('uriencode', str => encodeURL(str));
+    env.addFilter('formatUrl', str => full_url_for.call(hexo, str));
+    const template = nunjucks.compile(readFileSync('source/_feed.xml', 'utf8'), env);
     const data = template.render({config, url, icon, posts, feed_url});
     
     return {path, data};
