7270870135b0282359100e9cf325c124afd6edfb
luyuhuang <luyu_huang@foxmail.com>
2025-07-07 00:29:30 +0800

    modify injector

--- scripts/inject.js
+++ scripts/inject.js
@@ -1,16 +1,5 @@
 hexo.extend.filter.register('theme_inject', function(injects) {
+    injects.variable.push('source/_inject/variables')
     injects.postComments.file('default', 'source/_inject/comment.ejs');
     injects.linksComments.file('default', 'source/_inject/comment.ejs');
 });
-
-hexo.extend.injector.register('head_end', `<style>
-.markdown-body img {
-  box-shadow: unset !important;
-  border-radius: 0px !important;
-}</style>`)
-
-hexo.extend.injector.register('head_end',
-    `<link rel="alternate" href="/feed.xml" title="Luyu Huang's Blog" type="application/atom+xml">`)
-hexo.extend.injector.register('head_end',
-    `<link rel="sitemap" type="application/xml" title="Sitemap" href="/sitemap.xml">`)
-
